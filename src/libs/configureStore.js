48f8ddbf99444700c6e9e02f8c412e9e6ce31712
pivandrey <pivandrey777@gmail.com>
2019-08-06 20:08:00 +0600

    init project

---
+++ src/libs/configureStore.js
@@ -0,0 +1,24 @@
+import { createStore, applyMiddleware } from 'redux';
+import { rootReducer } from '../reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+const persistedState = sessionStorage.getItem('reduxState')
+  ? JSON.parse(sessionStorage.getItem('reduxState'))
+  : {};
+
+export const store = createStore(
+  rootReducer,
+  persistedState,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+store.subscribe(() => {
+    sessionStorage.setItem(
+    'reduxState',
+    JSON.stringify({
+      markers: store.getState().markers,
+      tags: store.getState().tags,
+    })
+  );
+});
