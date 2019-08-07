c6d11cf1c5ab136cd9db96a1735ba01e66f4e216
pivandrey <pivandrey777@gmail.com>
2019-08-07 07:38:45 +0600

    import reducers and add constant

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const CURRENCIES_URL = 'https://www.cbr-xml-daily.ru/daily_json.js';
+export const MAX_ITEMS_HISTORY = 10;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,8 @@
 import { combineReducers } from 'redux';
+import currencies from './currencies';
+import history from './history';
 
 export const rootReducer = combineReducers({
+  currencies,
+  history
 });
